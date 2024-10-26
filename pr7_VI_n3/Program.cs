25d26cec0bd9a65d62986e2b6c596a6136bfd360
Vadim Karasev <karasevvd@gmail.com>
2024-10-26 12:08:31 +0400

    задания по c#

--- pr7_VI_n3/Program.cs
+++ pr7_VI_n3/Program.cs
@@ -13,11 +13,10 @@ namespace pr7_VI_n3
             Console.Write("Введите количество строк: ");
             int rows = int.Parse(Console.ReadLine());
 
+            // Создаем ступенчатый массив
             int[][] array = new int[rows][];
 
             for (int i = 0; i < rows; i++) {
-                //Console.Write($"Введите количество элементов в строке {i + 1}: ");
-                //int cols = int.Parse(Console.ReadLine());
                 array[i] = new int[rows];
 
                 Console.WriteLine($"Введите элементы строки {i + 1} через пробел:");
@@ -28,16 +27,11 @@ namespace pr7_VI_n3
                 }
             }
 
-            // Новый список для хранения строк, включая добавленные
-            var resultList = new System.Collections.Generic.List<int[]>();
-
-            foreach (var row  in array)
+            // Перебор строк в массиве и вставка строк с нулями
+            for (int i = 0; i < array.Length; i++)
             {
-                resultList.Add(row);
-
-                // Проверка на четные элементы в стркое
                 bool hasEven = false;
-                foreach (var element in row)
+                foreach (var element in array[i])
                 {
                     if (element % 2 == 0)
                     {
@@ -45,15 +39,29 @@ namespace pr7_VI_n3
                         break;
                     }
                 }
+
+                // Если четных элементов нет, то вставляем строку с нулями
                 if (!hasEven)
                 {
-                    int[] zeroRow = new int[row.Length];
-                    resultList.Add(zeroRow);
+                    int[] zeroRow = new int[array[i].Length];
+
+                    // Увеличиваем размер массива и сдвигаем строки вниз
+                    Array.Resize(ref array, array.Length + 1);
+                    for (int j = array.Length - 1; j > i + 1; j--)
+                    {
+                        array[j] = array[j - 1];
+                    }
+
+                    // Вставляем строку с нулями после текущей строки
+                    array[i + 1] = zeroRow;
+
+                    // Пропускаем добавленную строку с нулями
+                    i++;
                 }
             }
-
+            // Вывод результата
             Console.WriteLine("Результат:");
-            foreach (var row in resultList)
+            foreach (var row in array)
             {
                 Console.WriteLine(string.Join(" ", row));
             }
---
+++ pr9_II_n18/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr9_II_n18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file1 = "file1.txt";
+            string file2 = "file2.txt";
+            string resultPath = "result.txt";
+
+            try
+            {
+                // Читаем числа из файлов и преобразуем в массивы
+                int[] numbers1 = File.ReadAllLines(file1).Select(int.Parse).ToArray();
+                int[] numbers2 = File.ReadAllLines(file2).Select(int.Parse).ToArray();
+
+                // Проверяем, что файлы имеют одинаковое количество чисел
+                if (numbers1.Length != numbers2.Length)
+                {
+                    Console.WriteLine("Файлы содержат разное количество чисел.");
+                    return;
+                }
+
+                using (StreamWriter writer = new StreamWriter(resultPath))
+                {
+                    for (int i = 0; i < numbers1.Length; i++)
+                    {
+                        int num1 = numbers1[i];
+                        int num2 = numbers2[i];
+
+                        // Проверяем делимость и записываем частное в новый файл, если делится
+                        if (num1 % num2 == 0)
+                        {
+                            writer.WriteLine(num1 / num2);
+                        }
+                        else if (num2 % num1 == 0)
+                        {
+                            writer.WriteLine(num2 / num1);
+                        }
+                    }
+                }
+
+                Console.WriteLine("Результат записан в файл: " + resultPath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+        }
+    }
+}
